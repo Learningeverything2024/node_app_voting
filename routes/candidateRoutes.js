d1bc8f3dfc3a22638aea5f062f3fc30ba3418ac4
Shivam Bansal <shivam1815916@gmail.com>
2024-10-06 17:09:20 +0530

    Update candidateRoutes.js

--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -70,7 +70,7 @@ router.delete('/:candidateId', jwtAuthMiddleware, async (req, res)=>{
         if(!checkAdminRole(req.user.id))
             return res.status(403).json({message: 'user does not have admin role'});
         
-        const candidateID = req.params.candidateID; // Extract the id from the URL parameter
+        const candidateId = req.params.candidateId; // Extract the id from the URL parameter
 
         const response = await Candidate.findByIdAndDelete(candidateId);
 
@@ -85,59 +85,12 @@ router.delete('/:candidateId', jwtAuthMiddleware, async (req, res)=>{
         res.status(500).json({error: 'Internal Server Error'});
     }
 })
-// update user password 
-router.put('/:candidateID', jwtAuthMiddleware, async (req, res)=>{
-    try{
-        if(!checkAdminRole(req.user.id))
-            return res.status(403).json({message: 'user does not have admin role'});
-        
-        const candidateID = req.params.candidateID; // Extract the id from the URL parameter
-        const updatedCandidateData = req.body; // Updated data for the person
-
-        const response = await Candidate.findByIdAndUpdate(candidateID, updatedCandidateData, {
-            new: true, // Return the updated document
-            runValidators: true, // Run Mongoose validation
-        })
-
-        if (!response) {
-            return res.status(404).json({ error: 'Candidate not found' });
-        }
-
-        console.log('candidate data updated');
-        res.status(200).json(response);
-    }catch(err){
-        console.log(err);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
-})
-// delete user
-router.delete('/:candidateID', jwtAuthMiddleware, async (req, res)=>{
-    try{
-        if(!checkAdminRole(req.user.id))
-            return res.status(403).json({message: 'user does not have admin role'});
-        
-        const candidateID = req.params.candidateID; // Extract the id from the URL parameter
-
-        const response = await Candidate.findByIdAndDelete(candidateID);
-
-        if (!response) {
-            return res.status(404).json({ error: 'Candidate not found' });
-        }
-
-        console.log('candidate deleted');
-        res.status(200).json(response);
-    }catch(err){
-        console.log(err);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
-})
-
 //voting 
-router.get('/voter/:candidateId', jwtAuthMiddleware,async (req,res)=>{
+router.get('/vote/:candidateId', jwtAuthMiddleware,async (req,res)=>{
 // no admin can vote
 // user can only vote once
 
-    candidateId = req.paras.candidateId;
+    candidateId = req.params.candidateId;
     userId = req.user.id;
 
     try{
